93eb08a03137e0d86a74f43433d8201f6c54f235
AndyPlov <plov.mail@gmail.com>
2023-12-10 19:30:24 -0600

    Add templates

--- dbHelper.py
+++ dbHelper.py
@@ -9,17 +9,17 @@ class dbHelper:
     def create_connection(self, path):
         connection = None
         try:
-            connection = sqlite3.connect(path)
+            connection = sqlite3.connect(path, timeout=10)
             print("Connection to SQLite DB successful")
         except Error as e:
             print(f"The error '{e}' occurred")
 
         return connection
     
-    def execute_query(self, connection, query):
+    def execute_query(self, connection, query, data):
         cursor = connection.cursor()
         try:
-            cursor.execute(query)
+            cursor.execute(query, data)
             connection.commit()
             print("Query executed successfully")
         except Error as e:
@@ -37,6 +37,18 @@ class dbHelper:
 
     def execute(self, query):
         connection = self.create_connection(self.path)
-        return self.execute_read_query(connection, query)
+        result = self.execute_read_query(connection, query)
+        if result == None :
+            return {}
+        else :
+            return result
+        
+    def executePush(self, query, data):
+        connection = self.create_connection(self.path)
+        result = self.execute_query(connection, query, data)
+        if result == None :
+            return {}
+        else :
+            return result
 
         
